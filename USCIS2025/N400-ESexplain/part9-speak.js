6dba05c2e9d4f5cbd3b35924ef819f83d1703345
textenergy <78602370+textenergy@users.noreply.github.com>
2026-06-11 13:58:28 -0400

    minor change to JS
    removed extra audio symbol from button

--- USCIS2025/N400-ESexplain/part9-speak.js
+++ USCIS2025/N400-ESexplain/part9-speak.js
@@ -20,7 +20,7 @@
         '<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3A4.5 4.5 0 0 0 14 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02z"/>' +
         '<path d="M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"/>' +
       '</svg>' +
-      (label || '&#x1F50A;&#xFE0E; Listen');
+      (label || 'Listen');
 
     btn.addEventListener('click', function () {
       if (synth.speaking) {
